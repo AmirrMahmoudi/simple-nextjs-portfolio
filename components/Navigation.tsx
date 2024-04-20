94613ef1e5b5fdca65d16854246a41f399e47336
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-20 19:18:22 +0330

    Navigation

---
+++ components/Navigation.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { NavLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+
+const Navigation = () => {
+  const [isRouting, setIsRouting] = useState(false);
+  const path = usePathname();
+  const [pervpath, setPervPath] = useState("/");
+
+  return (
+    <div
+      style={{ left: "20%" }}
+      className="absolute -bottom-20 z-[50] flex max-h-[150px] w-[50%] items-center justify-between rounded-full border border-white bg-black px-4 py-7 md:w-[20%]"
+    >
+      {isRouting&& <Transition/>}
+      {NavLinks.map((nav) => (
+        <Link key={nav.name} href={nav.link} className="mb-16 min-w-[20%] pl-4">
+          <nav.icon
+            className={`h-[24px] w-[24px] ${path === nav.name ? "text-purple-800" : "text-white"}`}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Navigation;
