2aebb5f4c45aa968801c763639deadae1ff559a9
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-04-20 19:51:50 +0330

    Transition

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,19 +2,37 @@
 import { NavLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Transition from "./Transition";
 
 const Navigation = () => {
   const [isRouting, setIsRouting] = useState(false);
   const path = usePathname();
-  const [pervpath, setPervPath] = useState("/");
+  const [pervPath, setPervPath] = useState("/");
+
+  useEffect(() => {
+    if(pervPath!==path){
+        setIsRouting(true)
+    }
+  }, [path,pervPath]);
+
+useEffect(()=>{
+    if(isRouting) {
+        setPervPath(path)
+     const timeout=setTimeout(()=>{
+     setIsRouting(false)
+   },1200)
+   return ()=> clearTimeout(timeout)
+    }
+},[isRouting])
+
 
   return (
     <div
       style={{ left: "20%" }}
       className="absolute -bottom-20 z-[50] flex max-h-[150px] w-[50%] items-center justify-between rounded-full border border-white bg-black px-4 py-7 md:w-[20%]"
     >
-      {isRouting&& <Transition/>}
+      {isRouting && <Transition />}
       {NavLinks.map((nav) => (
         <Link key={nav.name} href={nav.link} className="mb-16 min-w-[20%] pl-4">
           <nav.icon
